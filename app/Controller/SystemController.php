4f766de5d3bead700dde690074ff29d2ce1367db
Gavin Rogers <gavinrog@gmail.com>
2013-03-25 01:29:51 +0000

    more prog on auth

--- app/Controller/SystemController.php
+++ app/Controller/SystemController.php
@@ -5,31 +5,44 @@ class SystemController extends Controller {
 	public $components = array(
 		'Admin',
 		'Auth' => array(
+			'authenticate' => array(
+				'Form'
+			),
 			'authorize' => array(
 				'Controller'
 			),
-		)
+			'loginAction' => array(
+				'controller' => 'users',
+				'action' => 'login',
+				'admin' => false
+			),
+			'logoutUrl' => '/'
+		),
+		'Session'
 	);
-	
 	public $viewClass = 'System';
 
 	public function __construct($request = null, $response = null) {
-		
-		parent::__construct($request, $response);		
-;	}
-	
-	
-	
-	public function isAuthorized(){
-		die('test');
+		parent::__construct($request, $response);
 	}
 	
-	
+	public function beforeRender(){
+		$this->Auth->loginAction = array(
+			'controller' => 'users',
+			'action' => 'loginf',
+			'admin' => false
+		);
+	}
+
+	public function isAuthorized() {
+		return true;
+	}
+
 	public function invokeAction(CakeRequest $request) {
 		try {
-			parent::invokeAction($request);			
+			parent::invokeAction($request);
 		} catch (Exception $e) {
-			if($this->Admin->isAdmin()){
+			if ($this->Admin->isAdmin()) {
 				return true;
 			}
 			throw $e;
